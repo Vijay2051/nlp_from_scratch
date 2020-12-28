0780e430841a38c870d40c5961021acd50c24d9f
Vijay2051 <vijayvenu1997@gmail.com>
2020-12-28 20:32:57 +0000

    dataset for sentiment analysis completed

--- bert_based_nlp_abh_thk/models.py
+++ bert_based_nlp_abh_thk/models.py
@@ -1,6 +1,7 @@
 import transformers
 import torch.nn as nn
 
+
 class BertBaseUncased(nn.Module):
     def __init__(self) -> None:
         super(BertBaseUncased, self).__init__()
@@ -9,4 +10,12 @@ class BertBaseUncased(nn.Module):
         self.out = nn.Linear(768, 1)
 
     def forward(self, ids, mask, token_type_ids):
-        out1, out2 = self.bert(ids, attention_mask=mask, )
+        out1, out2 = self.bert(ids, attention_mask=mask, token_type_ids=token_type_ids)
+        """
+            :: out1 => sequence of hidden states for each token for all batches
+                        if you have 512 tokens, then you'll have 512 vectors of size 768 for each batch
+            :: out2 => contains the last layer hidden for the first cls token of the sequence
+        """
+        bert_drop = self.bert_drop(out2)
+        bert_out = self.out(bert_drop)
+        return bert_out
--- bert_based_nlp_abh_thk/trans.py
+++ bert_based_nlp_abh_thk/trans.py
@@ -10,6 +10,3 @@ random.seed(SEED)
 np.random.seed(SEED)
 torch.manual_seed(SEED)
 torch.backends.cudnn.deterministic = True
-
-# tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-# print(len(tokenizer.vocab))
